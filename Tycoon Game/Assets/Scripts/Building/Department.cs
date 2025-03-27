d57965b73c60e8ff91e3ff76bf6e958144498b81
MrCastro04 <slavamora684@gmail.com>
2025-03-27 20:06:25 +0200

    клік на префаби є

--- Tycoon Game/Assets/Scripts/Building/Department.cs
+++ Tycoon Game/Assets/Scripts/Building/Department.cs
@@ -1,9 +1,10 @@
+using Interfaces;
 using Scriptable_Objects;
 using UnityEngine;
 
 namespace Building
 {
-    public class Department : MonoBehaviour
+    public class Department : MonoBehaviour , IClickablePrefab
     {
         private DepartmentData _data;
 
@@ -12,6 +13,11 @@ namespace Building
         public void Init(DepartmentData data)
         {
             _data = data;
+        }
+
+        public void OnMouseDown()
+        {
+            Debug.Log("123");
         }
     }
 }
